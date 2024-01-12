52be6f679f0bc53c843861c761a9deb3b568de14
GecDeck <mithseager@gmail.com>
2024-01-12 04:22:22 -0500

    it disassembles better and now uses the correct addresses

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,9 @@ pub fn disassemble(data: &[u8]) -> Vec<Operation> {
             1 => println!("{:04x}   {:02x}          {}", address, op.op_code, op.instruction),
             2 => println!("{:04x}   {:02x} {:02x}       {}", address, op.op_code, op.data.0, op.instruction),
             3 => println!("{:04x}   {:02x} {:02x} {:02x}    {}", address, op.op_code, op.data.0, op.data.1, op.instruction),
-            _ => panic!(),
+            _ => panic!("Invalid number of bytes used for instruction"),
         }
-        address += 1;
+        address += op.op_bytes as u16;
     }
 
     return ops;
@@ -36,11 +36,12 @@ fn get_instruction_set() -> HashMap<u8, (String, u8)> {
         // Line should look like this
         // 0x(hex op code) (operation name) (number of bytes used for operation)
 
+        let (op_code_str, op): (&str, &str) = instruction_info.split_once(' ').expect("splitting op code from instruction");
+        let op_code: u8 = u8::from_str_radix(&op_code_str[2..=3], 16).expect("converting hex string slice to byte");
+        // Only using second half because the opcodes are written as 0x[8 bit code]
+
         if instruction_info.chars().last().unwrap() != '-' {
             // TODO: properly handle these non existent instructions
-            let (op_code_str, op): (&str, &str) = instruction_info.split_once(' ').expect("splitting op code from instruction");
-            let op_code: u8 = u8::from_str_radix(&op_code_str[2..=3], 16).expect("converting hex string slice to byte");
-            // Only using second half because the opcodes are written as 0x[8 bit code]
 
             let op_bytes: u8 = op.chars().last().expect("getting last char of op string which should be the number of bytes used in op")
                 .to_digit(10).expect("converting digit into u8") as u8;
@@ -50,6 +51,9 @@ fn get_instruction_set() -> HashMap<u8, (String, u8)> {
             // Trimming op_byte digit and whitespace off end
 
             instruction_set.insert(op_code, (String::from(instruction), op_bytes));
+        }
+        else {
+            instruction_set.insert(op_code, (String::from("-"), 1));
         }
     }
 
@@ -79,13 +83,18 @@ impl Operation {
 
 fn get_operation(data: &[u8], index: usize, instructions: &HashMap<u8, (String, u8)>) -> Operation {
     let op = match instructions.get(&data[index]) {
+        // Searching dictionary by op code
         Some((instruction, op_bytes)) => match op_bytes {
+            // Taking the correct number of bytes for the given instruction
             1 => Operation::new(instruction, data[index], *op_bytes, (0, 0)),
             2 => Operation::new(instruction, data[index], *op_bytes, (data[index+1], 0)),
             3 => Operation::new(instruction, data[index], *op_bytes, (data[index+2], data[index+1])),
-            _ => panic!(),
+            _ => panic!("There should never be an instruction with more than 3 bytes"),
         }
-        None => panic!(),
+        None => {
+            println!("No operation found for 0x{:02x}", data[index]);
+            panic!("Every byte should coorespond to an instruction");
+        },
     };
 
     return op;
